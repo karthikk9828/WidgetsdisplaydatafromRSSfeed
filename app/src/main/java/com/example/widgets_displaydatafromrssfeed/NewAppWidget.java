7a766dfc2e887b3b45fd200732809d714df4072f
Karthik K <karthikk9828@gmail.com>
2019-06-10 20:28:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/widgets_displaydatafromrssfeed/NewAppWidget.java
@@ -0,0 +1,43 @@
+package com.example.widgets_displaydatafromrssfeed;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class NewAppWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
+        final int numOfWidgets = appWidgetIds.length;
+
+        for (int i = 0; i < numOfWidgets; i++) {
+
+            int appWidgetId = appWidgetIds[i];
+
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+
+            Intent intent = new Intent(context, NewsService.class);
+            intent.putExtra("appWidgetId", appWidgetId);
+            context.startService(intent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
